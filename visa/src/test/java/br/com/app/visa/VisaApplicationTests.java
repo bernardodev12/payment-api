7af4845487da2b5f28285bed66f25ab35d91bbb7
Fernando Campos de Paiva <fernandocampospaiva@hotmail.com>
2024-07-10 18:13:53 -0300

    First commit

---
+++ visa/src/test/java/br/com/app/visa/VisaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app.visa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
